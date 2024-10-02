9325bf2ceb7e3e22be39a401d096d261afd24f84
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-10-03 06:26:22 +0800

    Game Greetings

---
+++ Hangman_Game.py
@@ -0,0 +1,20 @@
+# Hangman_Game
+
+#this_section_handles_the_greetings
+
+def greet_the_players():
+    print("Howdy Players!")
+    print("....")
+    print("Welcome to Hangman!")
+    print("....")
+    print("Your word category for today is...")
+    print("....")
+    print("fruits!")
+    print("....")
+    print("You may start, Good luck!")
+
+def main():
+    greet_the_players()
+
+if __name__ =="__main__":
+    main()
